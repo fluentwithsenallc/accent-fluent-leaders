fe3185f18987d36bac57909f64946074b669302a
Abdullah Omran <abdullah.omran1010@gmail.com>
2026-05-28 11:01:58 +0300

    testff

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Bm7zXbj6.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/3D/Desktop/Sena/accent-fluent-leaders/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-C6FwLZAT.js"] }, "/": { filePath: "C:/Users/3D/Desktop/Sena/accent-fluent-leaders/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CpkETI3E.js"] } }, clientEntry: "/assets/index-C6FwLZAT.js" });
+export {
+  tsrStartManifest
+};
